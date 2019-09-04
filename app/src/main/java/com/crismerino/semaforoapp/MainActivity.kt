d95d409d12a875b47dcdfc3301e4d433dcbc09f4
chrismerino <cris.merino@live.com>
2019-09-04 11:46:06 -0600

    Third commit / Falta el loop

--- app/src/main/java/com/crismerino/semaforoapp/MainActivity.kt
+++ app/src/main/java/com/crismerino/semaforoapp/MainActivity.kt
@@ -1,13 +1,38 @@
 package com.crismerino.semaforoapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // CREAR VARIABLES
+
+        val accionButton = findViewById<Button>(R.id.button_Iniciar)
+        val textViewTitulo = findViewById<TextView>(R.id.textView_Titulo)
+        val mainLayout = findViewById<ConstraintLayout>(R.id.mainLayout_Background)
+
+
+
+        // INSTANCIAR EL BOTON
+
+        accionButton.setOnClickListener {
+            accionButton.text = getString(R.string.text_ButtonParar)
+            textViewTitulo.text = getString(R.string.text_Adelante)
+            mainLayout.setBackgroundColor(Color.parseColor("#00E05E"))
+        }
+
+        // if
+
+
     }
 
 
