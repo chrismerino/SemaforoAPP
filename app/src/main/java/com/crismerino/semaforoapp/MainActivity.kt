337abe2e6791828b02f27e239e84dca3c8b2c589
chrismerino <cris.merino@live.com>
2019-09-05 23:43:47 -0600

    Fourth commit / Falta boton detener

--- app/src/main/java/com/crismerino/semaforoapp/MainActivity.kt
+++ app/src/main/java/com/crismerino/semaforoapp/MainActivity.kt
@@ -2,38 +2,61 @@ package com.crismerino.semaforoapp
 
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
+
 
 class MainActivity : AppCompatActivity() {
+
+    var i: Int = 0
+    lateinit var buttonInicio: Button
+    lateinit var textViewStatus: TextView
+    lateinit var mainLayout: LinearLayout
+    private lateinit var mHandler: Handler
+    private val mRunnable = object : Runnable{
+        override fun run(){
+            cambiosEnPantalla()
+            mHandler.postDelayed(this, 5000)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        buttonInicio = findViewById(R.id.button_Iniciar)
+        textViewStatus = findViewById(R.id.textViewStatus)
+        mainLayout = findViewById(R.id.mainLayout)
 
-        // CREAR VARIABLES
+        mHandler = Handler(Looper.getMainLooper())
 
-        val accionButton = findViewById<Button>(R.id.button_Iniciar)
-        val textViewTitulo = findViewById<TextView>(R.id.textView_Titulo)
-        val mainLayout = findViewById<ConstraintLayout>(R.id.mainLayout_Background)
-
-
-
-        // INSTANCIAR EL BOTON
-
-        accionButton.setOnClickListener {
-            accionButton.text = getString(R.string.text_ButtonParar)
-            textViewTitulo.text = getString(R.string.text_Adelante)
-            mainLayout.setBackgroundColor(Color.parseColor("#00E05E"))
+        buttonInicio.setOnClickListener {
+            mRunnable.run()
         }
-
-        // if
-
-
     }
 
-
+    fun cambiosEnPantalla(){
+        i = i+1
+        for (j in 1..3){
+            if (i == 1){
+                mainLayout.setBackgroundColor(Color.GREEN)
+                textViewStatus.text = "ADELANTE"
+            } else if (i == 2){
+                mainLayout.setBackgroundColor(Color.YELLOW)
+                textViewStatus.text = "PRECAUCION"
+            } else if (i == 3){
+                mainLayout.setBackgroundColor(Color.RED)
+                textViewStatus.text = "ALTO"
+            } else if (i >= 4){
+                i = 0
+            }
+        }
+    }
 }
+
+
+
